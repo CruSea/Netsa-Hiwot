58d26f5feace0ae1ddb48ef9692906798cf93385
Nat\Nat Crea <natiendex1@gmail.com>
2016-08-17 15:57:12 +0300

    add about us and contact us pages

--- Netsa Hiwot/app/src/main/java/com/solodroid/ecommerce/ActivityAbout.java
+++ Netsa Hiwot/app/src/main/java/com/solodroid/ecommerce/ActivityAbout.java
@@ -2,22 +2,44 @@ package com.solodroid.ecommerce;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityAbout extends Activity {
 	/** Called when the activity is first created. */
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about_us);
-		
+		Button facebookButton=(Button) findViewById(R.id.button1);
+
+
+		facebookButton.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View arg0)
+			{
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.facebook.com/netsahiwot"));
+				startActivity(browserIntent);
+
+				Intent tweet = new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.twetter.com/netsahiwot"));
+				startActivity(browserIntent);
+				Intent google = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com/netsahiwot"));
+				startActivity(browserIntent);
+			}
+		});
 		ActionBar bar = getActionBar();
-        bar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.header)));
-        bar.setTitle("About Us");
-        bar.setDisplayHomeAsUpEnabled(true);
-        bar.setHomeButtonEnabled(true);
+		bar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.header)));
+		bar.setTitle("About Us");
+		bar.setDisplayHomeAsUpEnabled(true);
+		bar.setHomeButtonEnabled(true);
 	}
+
+		
+
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {

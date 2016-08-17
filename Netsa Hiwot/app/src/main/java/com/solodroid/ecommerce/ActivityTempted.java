d9401745e61e1946dec90ca56d0fe9521ca35bf8
Sammie <samuelabate2@gmail.com>
2016-08-17 17:43:49 +0300

    Resolve the p[roblem

---
+++ Netsa Hiwot/app/src/main/java/com/solodroid/ecommerce/ActivityTempted.java
@@ -0,0 +1,74 @@
+package com.solodroid.ecommerce;
+
+import android.app.ActionBar;
+import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.MenuItem;
+
+public class ActivityTempted extends FragmentActivity {
+
+    ViewPager viewPager;
+    CRUDFragPagerAdapter cpg;
+    ActionBar bar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tempted);
+        init();
+        viewPager.setAdapter(cpg);
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                this.finish();
+                overridePendingTransition(R.anim.open_main, R.anim.close_next);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    void init()
+    {
+        Log.d("Hi Sammie", "Initializing inside ActivityTempted...");
+        bar = getActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.header)));
+        bar.setTitle("Tempted");
+        bar.setDisplayHomeAsUpEnabled(true);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        cpg = new CRUDFragPagerAdapter(getSupportFragmentManager());
+
+        //bar.setHomeButtonEnabled(true);
+        Log.d("Hi Sammie", "Initializing finished for ActivityTempted...");
+    }
+
+    // when back button pressed close database and back to previous page
+    @Override
+    public void onBackPressed() {
+        // TODO Auto-generated method stub
+        super.onBackPressed();
+        finish();
+        overridePendingTransition(R.anim.open_main, R.anim.close_next);
+    }
+
+    @Override
+    public void onConfigurationChanged(final Configuration newConfig) {
+        // Ignore orientation change to keep activity from restarting
+        super.onConfigurationChanged(newConfig);
+    }
+}
